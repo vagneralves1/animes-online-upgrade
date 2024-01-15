4226334c19945e02562597f8e6b3d4958d5b780f
vagneralves1 <vagner.alves.js@gmail.com>
2024-01-14 21:52:21 -0300

    salve alfabeto

--- java_animes-online.js
+++ java_animes-online.js
@@ -1,13 +1,14 @@
 var menu = document.getElementById('menu_oculto')
-var icon = document.getElementsByClassName('icon-s')
-var check = 0                               
+var check = 0  
 
 function clicou_menu(){
     if (check == 0){
         menu.style.display = 'block'
         check = 1
+        document.getElementById('icon').src = 'imagens/close_white_36dp.svg'
     }else{
         check = 0
         menu.style.display = 'none'
+        document.getElementById('icon').src = 'imagens/menu_white_36dp.svg'
     }
 }
